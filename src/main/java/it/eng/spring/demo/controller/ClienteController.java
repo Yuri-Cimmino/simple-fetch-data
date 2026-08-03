5c5da87659b6838a436af029c11c57bf3d93e4ee
soliveri <soliveri@SOLIVERIW1.homenet.telecomitalia.it>
2026-08-03 12:07:10 +0200

    Merge branch 'main' of https://github.com/Yuri-Cimmino/simple-fetch-data.git into main

--- src/main/java/it/eng/spring/demo/controller/ClienteController.java
+++ src/main/java/it/eng/spring/demo/controller/ClienteController.java
@@ -3,6 +3,7 @@ package it.eng.spring.demo.controller;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.eng.spring.demo.model.Cliente;
@@ -28,5 +29,38 @@ public class ClienteController {
 		
 		return listaClienti;
 	}
+	
+	@GetMapping("/cliente/{id}")
+	public Cliente getSpecificClient(@PathVariable Long id) {
+	 Cliente cliente1 = new Cliente("Lucia","Rossi",id);
+	 return cliente1;
+	}
+	
+	@GetMapping("/clienti/{id}")
+	public Cliente getSpecificClients(@PathVariable Long id) {
+		Cliente cliente2 = new Cliente("Antonio","Falco",2l);
+		Cliente cliente1 = new Cliente("Lucia","Rossi",1l);
+		
+		ArrayList<Cliente> listaClienti = new ArrayList<Cliente>();
+		
+		listaClienti.add(cliente2);
+		listaClienti.add(cliente1);
+		
+		
+		Cliente trovato = null;
+
+		for(int i = 0; i < listaClienti.size(); i++ ) {
+			Cliente c = listaClienti.get(i);
+			
+			if(c.getId() == id){
+				trovato = c;
+				break;
+			}
+			
+		}
+		return trovato;
+		
+	}
+	
 
 }
