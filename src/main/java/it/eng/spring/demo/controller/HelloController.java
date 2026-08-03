f630905520716ac67dda48d7a065345da66ebaa1
soliveri <soliveri@SOLIVERIW1.homenet.telecomitalia.it>
2026-08-03 10:15:52 +0200

    Added ciao controller

---
+++ src/main/java/it/eng/spring/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package it.eng.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping("/ciao")
+	public String ciao() {
+		return "Ciao dal Controller";
+	}
+
+}
